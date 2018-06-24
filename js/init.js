5f34daf6e1fb43fac18efa9e023a41a3365fc7be
Luis F Suárez Harašić <suarezluis@gmail.com>
2018-06-24 00:22:55 -0500

    added portfolio

--- js/init.js
+++ js/init.js
@@ -8,3 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
