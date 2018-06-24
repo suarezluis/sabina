19f67e618e6d44861c136339ba4ef9b465f3923d
Luis F Suárez Harašić <suarezluis@gmail.com>
2018-06-23 22:27:38 -0500

    new layout per client request

---
+++ js/init.js
@@ -0,0 +1,10 @@
+
+
+  // materialize parallax option
+var options = {}
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options );
+  });
+
+
