b5fd9b983d39a79e69be52c3b2743aeb2abdab41
Luis F Suárez Harašić <suarezluis@gmail.com>
2018-06-24 00:28:50 -0500

    format code

--- js/init.js
+++ js/init.js
@@ -1,14 +1,11 @@
+// materialize parallax option
+var options = {};
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".parallax");
+  var instances = M.Parallax.init(elems, options);
+});
 
-
-  // materialize parallax option
-var options = {}
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options );
-  });
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems, options);
+});
